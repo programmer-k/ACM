ec832b8591f5aabd00d3f69b86a7966b34c195b7
programmer-k <programmer-j@naver.com>
2022-09-18 04:23:02 +0900

    Add dotenv and function for geocoding request.

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
+require('dotenv').config();
 const xlsx = require("xlsx");
 const express = require('express')
+const request = require('request')
+
 
 const file = xlsx.readFile("병역지정업체검색_20220918.xls")
-
 const app = express();
+
+
+function requestGeocoding() {
+    const address = encodeURI('서울특별시 중구 퇴계로22길11-8');
+    const options = {
+        uri:'https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=' + address,
+        headers: {
+            'X-NCP-APIGW-API-KEY-ID': process.env.KEY_ID,
+            'X-NCP-APIGW-API-KEY': process.env.KEY
+        }
+      }
+      request(options, (err, response, body) => {
+        //console.log(err);
+        //console.log(response);
+        console.log(body);
+      });
+}
+
 
 app.get('/', function (req, res) {
     // Set the header
@@ -19,6 +39,8 @@ app.get('/', function (req, res) {
     res.end();
 });
 
+
 app.listen(3000, () => {
     console.log("Starting the server..")
+    requestGeocoding();
 });
